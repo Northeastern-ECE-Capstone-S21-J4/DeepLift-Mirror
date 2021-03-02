a43999570f7c3f06c47d4c996e23a8a248bb2709
Nicholas Fresneda <nicholasfresneda@gmail.com>
2021-03-02 18:12:38 -0500

    changed script to run

--- run_trt_pose.sh
+++ run_trt_pose.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 
-nvidia-docker run --device /dev/video0 -v /home/deeplift/container_share/:/container_share -p 8888:8888 -it 7ee7bd70fce1 jupyter notebook /container_share/trt_pose/tasks/human_pose/live_demo.ipynb --allow-root --ip 0.0.0.0
+sudo nvidia-docker run --device /dev/video0 -v /home/deeplift/container_share/:/container_share -p 8888:8888 -it 7ee7bd70fce1 jupyter notebook /container_share/trt_pose/tasks/human_pose/live_demo.ipynb --allow-root --ip 0.0.0.0
